40103af6b216b2ac145b8993359ac5965b8f5ae6
Eldar Sayranov <sayranoveldar@gmail.com>
2025-03-13 17:54:17 +0500

    add custom font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,5 +19,22 @@ export default defineNuxtConfig({
   eslint: {
     stylistic: {}
   },
-  compatibilityDate: '2025-02-01'
+  compatibilityDate: '2025-02-01',
+  app: {
+    link: [
+      {
+        rel: 'preconnect',
+        href: 'https://fonts.googleapis.com'
+      },
+      {
+        rel: 'preconnect',
+        href: 'https://fonts.gstatic.com',
+        crossorigin: 'anonymous'
+      },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap'
+      }
+    ]
+  }
 })
